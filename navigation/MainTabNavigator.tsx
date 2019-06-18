5fa93fee94b89b1d2c7dbd3295b79ba42bbeadeb
csteinberg <csteinberg@geekhive.com>
2019-06-18 13:56:42 -0400

    Initial Commit

---
+++ navigation/MainTabNavigator.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+  TabBarIconProps,
+} from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: (props: TabBarIconProps) => (
+    <TabBarIcon
+      focused={props.focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${props.focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const LinksStack = createStackNavigator({
+  Links: LinksScreen,
+});
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Links',
+  tabBarIcon: (props: TabBarIconProps) => (
+    <TabBarIcon
+      focused={props.focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: (props: TabBarIconProps) => (
+    <TabBarIcon
+      focused={props.focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack,
+});
