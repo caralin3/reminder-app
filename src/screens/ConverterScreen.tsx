ff6f10c2bef57440b152b106843a54ec39bf0ba7
Cara Steinberg <caralin3@gmail.com>
2019-06-20 15:24:12 -0400

    Clean up code

---
+++ src/screens/ConverterScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const ConverterScreen = () => {
+  return (
+    <View>
+      <Text>Converter</Text>
+    </View>
+  );
+};
+
+ConverterScreen.navigationOptions = {
+  title: 'Convert Dates'
+};
